6a501fc440a5170661a937229aaec18028c1ac3e
drlssnlvss <darlysson.santana@querodelivery.com>
2020-08-14 01:02:18 -0300

    auth

--- src/instagram.ts
+++ src/instagram.ts
@@ -2,16 +2,31 @@ import puppeteer from "puppeteer";
 
 import { BASE_ULR } from "./consts";
 
-let browser = null;
-let page = null;
+let browser: any = null;
+let page: any = null;
 
 class Instagram {
   init = async (): Promise<void> => {
     browser = await puppeteer.launch({ headless: false });
 
     page = await browser.newPage();
+  };
 
-    await page.goto(BASE_ULR, { waitUntil: "networkidle2" });
+  auth = async (user: any, pass: any): Promise<void> => {
+    await page.goto(`${BASE_ULR}accounts/login/?source=auth_switcher`, {
+      waitUntil: "networkidle2",
+    });
+
+    await page.waitFor(2500);
+
+    await page.type("input[name='username']", user);
+    await page.type("input[name='password']", pass);
+
+    const confirmLogin = await page.$x(
+      "/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div/div[3]/button/div"
+    );
+
+    await confirmLogin[0].click();
   };
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import { config } from "dotenv";
 import Instagram from "./instagram";
+
+config();
 
 const instagram = new Instagram();
 
 async function launch() {
   await instagram.init();
+
+  await instagram.auth(process.env.INSTA_USER, process.env.INSTA_PASS);
 }
 
 launch();
